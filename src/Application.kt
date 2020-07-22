a7e175ec6a108ad993c6b8899988a58a7f86639a
Keith Lazuka <klazuka@gmail.com>
2020-07-21 20:00:14 -0400

    Enable class reloading

--- src/Application.kt
+++ src/Application.kt
@@ -1,14 +1,15 @@
 package com.klazuka
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.html.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.html.respondHtml
+import io.ktor.http.ContentType
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 import kotlinx.html.*
-import io.ktor.content.*
-import io.ktor.http.content.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -29,6 +30,15 @@ fun Application.module(testing: Boolean = false) {
                             li { +"$n" }
                         }
                     }
+                }
+            }
+        }
+
+        get("/decks") {
+            call.respondHtml {
+                body {
+                    h1 { +"Decks" }
+                    p { +"fire" }
                 }
             }
         }
