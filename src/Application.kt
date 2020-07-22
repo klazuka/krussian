466e328b01c50a446bfd2a8d367ae5bc9af8fefb
Keith Lazuka <klazuka@gmail.com>
2020-07-22 14:08:01 -0400

    Start building the content and using Bootstrap

--- src/Application.kt
+++ src/Application.kt
@@ -3,10 +3,8 @@ package com.klazuka
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.html.respondHtml
-import io.ktor.http.ContentType
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
-import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import kotlinx.html.*
@@ -18,13 +16,21 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
-
-        get("/html-dsl") {
             call.respondHtml {
                 body {
-                    h1 { +"HTML" }
+                    h1 { +"Home" }
+                    p { a(href = "/decks/all") { +"All Decks" } }
+                    p { a(href = "/decks/due") { +"Due Decks" } }
+                    p { a(href = "/resources") { +"Resources" } }
+                }
+            }
+
+        }
+
+        get("/decks/all") {
+            call.respondHtml {
+                body {
+                    h1 { +"All Decks" }
                     ul {
                         for (n in 1..10) {
                             li { +"$n" }
@@ -34,11 +40,44 @@ fun Application.module(testing: Boolean = false) {
             }
         }
 
-        get("/decks") {
+        get("/decks/due") {
             call.respondHtml {
                 body {
-                    h1 { +"Decks" }
+                    h1 { +"Due Decks" }
                     p { +"fire" }
+                }
+            }
+        }
+
+        get("/resources") {
+            call.respondHtml {
+                head {
+                    link(rel = "stylesheet", href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css")
+                }
+                body {
+                    div(classes = "container") {
+                        div(classes = "card") {
+                            div(classes = "card-body") {
+                                h5(classes = "card-title") { +"Links" }
+                                p {
+                                    a(href = "https://russianwithmax.com") { +"RussianWithMax" }
+                                    +" - podcast for non-native speakers"
+                                }
+                                p {
+                                    a(href = "https://www.orusskomporusski.com") { +"О русском по-русски" }
+                                    +" - YouTube channel for Russian grammar, vocab, etc."
+                                }
+                                p {
+                                    a(href = "https://meduza.io") { +"Meduza" }
+                                    +" - news, podcasts"
+                                }
+                                p {
+                                    a(href = "https://arzamas.academy") { +"Arzamas" }
+                                    +" - art, culture, history, podcasts"
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
